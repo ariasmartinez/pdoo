7ef629f6d0ffc6d0a47c7029fcdb4306785351a1
Celia Arias <ariasmartinez@correo.ugr.es>
2019-02-26 16:15:39 +0100

    JAVA subo Dice

--- PRACTICA1/JAVA/deepspace/src/deepspace/Dice.java
+++ PRACTICA1/JAVA/deepspace/src/deepspace/Dice.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package deepspace;
+// buscado en internet
+import java.util.Random;
 
 /**
  *
@@ -17,11 +19,55 @@ class Dice {
     private Random generator;
     
     Dice(){
-        // inicializar?? generator = 
+        //lo dice internet
+        generator = new Random(System.currentTimeMillis());
         NHANGARSPROB=0.25f;
         NSHIELDSPROB=0.25f;
         NWEAPONSPROB=0.33f;
         FIRSTSHOTPROB=0.5f;
+    }
+    
+    int initWithNHangars(){
+        //no se si hay que ponerlo
+        //generator.setSeed(System.currentTimeMillis());
+        if (generator.nextFloat() < NHANGARSPROB)
+            return 0;
+        return 1;
+    }
+    
+    int initWithNWeapons(){
+        float aleat = generator.nextFloat();
+        if ( aleat < NWEAPONSPROB)
+            return 1;
+        else if(aleat < 2*NWEAPONSPROB)
+            return 2;
+        else 
+            return 3;
+    }
+    
+    int initWithNShields(){
+        if (generator.nextFloat() < NSHIELDSPROB)
+           return 0;
+        return 1;
+    }
+    
+    int whoStarts(int nPlayers){
+        return generator.nextInt(nPlayers);
+    }
+    
+    GameCharacter firstShot(){
+        GameCharacter personaje;
+        if (generator.nextFloat() < FIRSTSHOTPROB)
+            personaje = GameCharacter.SPACESTATION; 
+        else
+            personaje = GameCharacter.ENEMYSTARSHIP;
+        return personaje;    
+    }
+    
+    boolean spaceStationMoves(float speed){
+        if (generator.nextFloat() < speed)
+           return true;
+        return false;
     }
     
 }
