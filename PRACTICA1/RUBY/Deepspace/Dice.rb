be34491e8314fe5d5bfd8d956b52d02e53bee931
Lucía Salamanca <luciasalamanca@github.com>
2019-03-07 02:14:26 +0100

    Cambio Dice.rb me falta solo un método que he preguntado por el grupo

--- PRACTICA1/RUBY/Deepspace/Dice.rb
+++ PRACTICA1/RUBY/Deepspace/Dice.rb
@@ -9,7 +9,56 @@ class Dice
    @generator=rand
  end
 
+# Para hacer los consultores directamente no sé si funciona
+# Si no funciona el fallo está aquí
+attr_reader :NHANGARSPROB, :NSHIELDSPROB, :NWEAPONSPROB, :FIRSTSHOTPROB, :generator
 
+ def initWithNHangars
+   if (generator > NHANGARSPROB )
+     return 1
+   else
+     return 0
+   end
+
+ end
+
+ def initWithNWeapons
+   if (generator > NWEAPONSPROB)
+     return 1
+
+   else
+   end
+
+ end
+
+ def initWithNShields
+   if (generator > NSHIELDSPROB )
+     return 1
+   else
+     return 0
+   end
+ end
+
+ def whoStarts(nPlayers)
+   rand(nPlayers)
+ end
+
+ def firstShot
+   if (generator > FIRSTSHOTPROB )
+     return Deepspace::GameCharacter::ENEMYSTARSHIP
+   else
+     return Deepspace::GameCharacter::SPACESTATION
+   end
+
+ end
+
+ def spaceStationMoves(speed)
+   if (generator > speed)
+     return false 
+   else
+     return true
+   end
+ end
 
 
 
