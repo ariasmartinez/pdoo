49746389dd3eb2d4688d1a5890aa3d28662b4067
Lucía Salamanca <luciasalamanca@github.com>
2019-05-30 19:54:47 +0200

    Arreglo ruby

--- PRACTICA1/RUBY/Deepspace/SpaceCity.rb
+++ PRACTICA1/RUBY/Deepspace/SpaceCity.rb
@@ -4,8 +4,7 @@ module Deepspace
 # @author Lucía Salamanca López, Celia Arias Martínez
 class SpaceCity < SpaceStation
     def initialize(base, rest)   #base es SpaceStation original y rest es Array de SpaceStation (las demás estaciones)
-        copy(base)
-        @base = base #SpaceStation 
+        copy(base)  
         @collaborators = rest #array de SpaceStation, queremos el puntero
     end 
 
@@ -15,7 +14,7 @@ class SpaceCity < SpaceStation
 
     
     def fire 
-        suma = @base.fire   #creo que es lo mismo que super.fire
+        suma = super.fire   #creo que es lo mismo que super.fire
         for f in collaborators
             suma+=f.fire
         end
@@ -24,7 +23,7 @@ class SpaceCity < SpaceStation
 
     
     def protection
-        suma = @base.protection  #creo que es lo mismo que super.protection
+        suma = super.protection  #creo que es lo mismo que super.protection
         for pr in collaborators
             suma+=pr.protection
         end
@@ -41,7 +40,7 @@ class SpaceCity < SpaceStation
     end
 
     def to_s  
-        "La estación espacial original es \n" + @base.to_s + "\n y las estaciones espaciales colaboradoras son\n #{@collaborators.join("\n ")}."
+        "La estación espacial original es \n" + super.to_s + "\n y las estaciones espaciales colaboradoras son\n #{@collaborators.join("\n ")}."
     end
 end 
 
